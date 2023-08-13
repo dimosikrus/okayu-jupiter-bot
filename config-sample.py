4e1526405385306c17d30e1f5bf412f5d1b3b478
dimosikrus <30956599+dimosikrus@users.noreply.github.com>
2023-08-13 23:35:29 +0500

    Create config-sample.py

---
+++ config-sample.py
@@ -0,0 +1,20 @@
+#config
+
+DOMAIN = "example.com"
+BOTNAME = 'BOTNAME'
+TOKEN = "TOKEN"
+COLOR = "000000"
+SERVERID = 895724040923258952
+
+#how to get discord emoji id
+# type \ than write emoji
+
+RANK_SSH = "<:rank_ssh:1062795235849146379>"
+RANK_SS = "<:rank_ss:1062795191246925934>"
+RANK_SH = "<:rank_sh:1062795147403866183>"
+RANK_S = "<:rank_s:1062795271005798420>"
+RANK_A = "<:rank_a:1062795378874912870>"
+RANK_B = "<:rank_b:1062795299824869396>"
+RANK_C = "<:rank_c:1062795328652316834>"
+RANK_D = "<:rank_d:1062795354992541706>"
+RANK_F = "<:rank_f:1062794995435835464>"
